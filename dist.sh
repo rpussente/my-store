e64badcb11c26d8a689d0a29a42c9bbf2094febc
Rodolfo Pussente <rodolfo.pussente@gmail.com>
2019-02-02 10:46:45 +0100

    Set wine storage folder

--- dist.sh
+++ dist.sh
@@ -10,6 +10,7 @@ docker pull electronuserland/builder:wine
 
 docker run --rm -ti \
   --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
+  --env WINEPREFIX="/root/.wine" \
   --env ELECTRON_CACHE="/root/.cache/electron" \
   --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
   -v ${PWD}:/project \
