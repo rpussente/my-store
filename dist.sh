3c9a58344ec2e56259e41b3171641c65d6292ce3
Rodolfo Pussente <rodolfo.pussente@gmail.com>
2019-02-02 10:59:56 +0100

    Build and dist in different steps

--- dist.sh
+++ dist.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-echo "${PWD}"
-echo "${PWD##*/}-node-modules"
-echo <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_')
-ls "/root/.cache/electron"
-ls "/root/.cache/electron-builder"
-
 docker pull electronuserland/builder:wine
 
 docker run --rm -ti \
@@ -17,4 +11,4 @@ docker run --rm -ti \
   -v ${PWD##*/}-node-modules:/project/node_modules \
   -v ~/.cache/electron:/root/.cache/electron \
   -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-  electronuserland/builder:wine /bin/bash -c "yarn install && yarn build && yarn dist"
+  electronuserland/builder:wine /bin/bash -c "yarn dist"
