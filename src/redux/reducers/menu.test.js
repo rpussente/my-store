05c870b8b6e66351ef975c82c9f158d37b0d9d8e
Rodolfo Pussente <rodolfo.pussente@gmail.com>
2019-05-10 00:39:43 +0200

    Add tests to redux actions and reducers

---
+++ src/redux/reducers/menu.test.js
@@ -0,0 +1,28 @@
+import reducer from '../reducers/menu'
+import * as types from '../constants/action-types'
+
+describe('menu reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      open: true
+    })
+  })
+
+  it('should handle TOGGLE_MENU', () => {
+    expect(
+      reducer({ open: true }, {
+        type: types.TOGGLE_MENU
+      })
+    ).toEqual({
+      open: false
+    })
+
+    expect(
+      reducer({ open: false }, {
+        type: types.TOGGLE_MENU
+      })
+    ).toEqual({
+      open: true
+    })
+  })
+})
